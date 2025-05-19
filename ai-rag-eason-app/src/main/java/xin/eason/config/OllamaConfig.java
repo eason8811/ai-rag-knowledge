fa65ac89e44c2d9c8a89a13fa30e3ecda163056d
Eason <3220563569@qq.com>
2025-05-19 14:34:22 +0800

    完成 OpenAI 根据给定 RAG 知识库进行流式对话的 API 接口, 以及不根据 RAG 知识库进行对话的流式和非流式接口

--- ai-rag-eason-app/src/main/java/xin/eason/config/OllamaConfig.java
+++ ai-rag-eason-app/src/main/java/xin/eason/config/OllamaConfig.java
@@ -5,13 +5,18 @@ import org.springframework.ai.ollama.OllamaChatClient;
 import org.springframework.ai.ollama.OllamaEmbeddingClient;
 import org.springframework.ai.ollama.api.OllamaApi;
 import org.springframework.ai.ollama.api.OllamaOptions;
+import org.springframework.ai.openai.OpenAiEmbeddingClient;
+import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.PgVectorStore;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.Optional;
 
 /**
  * Ollama 模型配置注入类
@@ -21,10 +26,14 @@ import org.springframework.jdbc.core.JdbcTemplate;
 @EnableConfigurationProperties(OllamaConfigProperties.class)
 public class OllamaConfig {
 
+    /**
+     * @see OllamaConfigProperties
+     */
     private final OllamaConfigProperties ollamaConfigProperties;
 
     /**
      * 从配置文件读取 base-url, 然后新建并注入 {@link  OllamaApi} 对象
+     *
      * @return {@link  OllamaApi} 的 Bean 对象
      */
     @Bean
@@ -34,6 +43,7 @@ public class OllamaConfig {
 
     /**
      * 根据上面注入的 {@link  OllamaApi} 对象, 新建并注入 {@link  OllamaChatClient} 对象
+     *
      * @param ollamaApi 注入的 {@link  OllamaApi} 的 Bean 对象
      * @return {@link  OllamaChatClient} 的 Bean 对象
      */
@@ -44,6 +54,7 @@ public class OllamaConfig {
 
     /**
      * 注入 Token 文本分割器对象
+     *
      * @return Token 文本分割器 Bean
      */
     @Bean
@@ -52,11 +63,13 @@ public class OllamaConfig {
     }
 
     /**
-     * 注入 {@link SimpleVectorStore} 简易向量存储器对象, 用于本地存储向量信息
+     * 注入 {@link SimpleVectorStore} 简易向量存储器对象, 用于本地存储向量信息 ( 嵌入模型: nomic-embed-text )
+     *
      * @param ollamaApi {@link  OllamaApi} 的 Bean 对象
      * @return {@link SimpleVectorStore} Bean 对象
      */
     @Bean
+    @ConditionalOnProperty(prefix = "spring.ai.rag", name = "embed", havingValue = "nomic-embed-text", matchIfMissing = true)
     public SimpleVectorStore simpleVectorStore(OllamaApi ollamaApi) {
         OllamaEmbeddingClient ollamaEmbeddingClient = new OllamaEmbeddingClient(ollamaApi);
         ollamaEmbeddingClient.withDefaultOptions(OllamaOptions.create().withModel("nomic-embed-text"));
@@ -64,17 +77,45 @@ public class OllamaConfig {
     }
 
     /**
-     * 注入 {@link PgVectorStore} 可使用数据库存储的向量储存器对象
-     * @param ollamaApi {@link  OllamaApi} 的 Bean 对象
+     * 注入 {@link SimpleVectorStore} 简易向量存储器对象, 用于本地存储向量信息 ( 嵌入模型: text-embedding-ada-002 )
+     *
+     * @param openAiApi {@link  OpenAiApi} 的 Bean 对象
+     * @return {@link SimpleVectorStore} Bean 对象
+     */
+    @Bean
+    @ConditionalOnProperty(prefix = "spring.ai.rag", name = "embed", havingValue = "text-embedding-ada-002")
+    public SimpleVectorStore simpleVectorStore(OpenAiApi openAiApi) {
+        OpenAiEmbeddingClient openAiEmbeddingClient = new OpenAiEmbeddingClient(openAiApi);
+        return new SimpleVectorStore(openAiEmbeddingClient);
+    }
+
+    /**
+     * 注入 {@link PgVectorStore} 可使用数据库存储的向量储存器对象 ( 嵌入模型: nomic-embed-text )
+     *
+     * @param ollamaApi    {@link  OllamaApi} 的 Bean 对象
      * @param jdbcTemplate 用于调用数据库的对象
      * @return {@link PgVectorStore} Bean 对象
      */
     @Bean
+    @ConditionalOnProperty(prefix = "spring.ai.rag", name = "embed", havingValue = "nomic-embed-text", matchIfMissing = true)
     public PgVectorStore pgVectorStore(OllamaApi ollamaApi, JdbcTemplate jdbcTemplate) {
         OllamaEmbeddingClient ollamaEmbeddingClient = new OllamaEmbeddingClient(ollamaApi);
         ollamaEmbeddingClient.withDefaultOptions(OllamaOptions.create().withModel("nomic-embed-text"));
         return new PgVectorStore(jdbcTemplate, ollamaEmbeddingClient);
     }
 
+    /**
+     * 注入 {@link PgVectorStore} 可使用数据库存储的向量储存器对象 ( 嵌入模型: text-embedding-ada-002 )
+     *
+     * @param openAiApi    {@link  OpenAiApi} 的 Bean 对象
+     * @param jdbcTemplate 用于调用数据库的对象
+     * @return {@link PgVectorStore} Bean 对象
+     */
+    @Bean
+    @ConditionalOnProperty(prefix = "spring.ai.rag", name = "embed", havingValue = "text-embedding-ada-002")
+    public PgVectorStore pgVectorStore(OpenAiApi openAiApi, JdbcTemplate jdbcTemplate) {
+        OpenAiEmbeddingClient openAiEmbeddingClient = new OpenAiEmbeddingClient(openAiApi);
+        return new PgVectorStore(jdbcTemplate, openAiEmbeddingClient);
+    }
 
 }
---
+++ ai-rag-eason-trigger/src/main/java/xin/eason/trigger/http/OpenAiController.java
@@ -0,0 +1,97 @@
+package xin.eason.trigger.http;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.openai.OpenAiChatClient;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.ai.vectorstore.PgVectorStore;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import xin.eason.api.IAiService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/v1/openai")
+@RequiredArgsConstructor
+public class OpenAiController implements IAiService {
+
+    /**
+     * OpenAI 的对话客户端
+     */
+    private final OpenAiChatClient openAiChatClient;
+
+    /**
+     * PostgreSQL 向量库
+     */
+    private final PgVectorStore pgVectorStore;
+
+    /**
+     * 根据消息生成响应结果
+     *
+     * @param model   需要使用的模型
+     * @param message 发送的消息
+     * @return 返回 Spring AI 对话响应对象 ( 非流式传输 )
+     */
+    @Override
+    @GetMapping("/generate")
+    public ChatResponse generate(String model, String message) {
+        return openAiChatClient.call(new Prompt(message, OpenAiChatOptions.builder().withModel(model).build()));
+    }
+
+    /**
+     * 根据消息生成响应结果
+     *
+     * @param model   需要使用的模型
+     * @param message 发送的消息
+     * @return 返回 Spring AI 对话响应对象 ( 流式传输 )
+     */
+    @Override
+    @GetMapping("/generate_stream")
+    public Flux<ChatResponse> generateStream(String model, String message) {
+        return openAiChatClient.stream(new Prompt(message, OpenAiChatOptions.builder().withModel(model).build()));
+    }
+
+    /**
+     * 根据选定的 知识库Tag 流式生成消息的响应结果
+     *
+     * @param model   需要使用的模型
+     * @param ragTag  知识库 Tag
+     * @param message 发送的消息
+     * @return 返回 Spring AI 对话响应对象 ( 流式传输 )
+     */
+    @Override
+    @GetMapping("/generate_stream_rag")
+    public Flux<ChatResponse> generateStreamWithRag(String model, String ragTag, String message) {
+        String SYSTEM_PROMPT = """
+                Use the information from the DOCUMENTS section to provide accurate answers but act as if you knew this information innately.
+                If unsure, simply state that you don't know.
+                Another thing you need to note is that your reply must be in Chinese!
+                DOCUMENTS:
+                    {documents}
+                """;
+        SearchRequest searchRequest = SearchRequest.query(message).withTopK(10).withFilterExpression("knowledge == '" + ragTag + "'");
+        List<Document> documents = pgVectorStore.similaritySearch(searchRequest);
+        String documentString = documents.stream().map(Document::getContent).collect(Collectors.joining());
+        Message systemMsg = new SystemPromptTemplate(SYSTEM_PROMPT).createMessage(Map.of("documents", documentString));
+        Message userMessage = new UserMessage(message);
+
+        List<Message> messageList = new ArrayList<>();
+        messageList.add(systemMsg);
+        messageList.add(userMessage);
+        return openAiChatClient.stream(new Prompt(messageList, OpenAiChatOptions.builder().withModel(model).build()));
+    }
+}
